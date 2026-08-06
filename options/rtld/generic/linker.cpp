84c236d75d956dd3bc7c09eb21820758a0af7c42
cpplover0 <osdev555@yandex.com>
2026-08-06 18:06:23 +0300

    ggg

--- options/rtld/generic/linker.cpp
+++ options/rtld/generic/linker.cpp
@@ -1180,17 +1180,17 @@ void ObjectRepository::_discoverDependencies(SharedObject *object,
 			mlibc::panicLogger() << "Could not satisfy dependency " << library_str << frg::endlog;
 
 		auto library = libraryResult.value();
-		mlibc::panicLogger() << "5555999 " << library_str << frg::endlog;
+		mlibc::infoLogger() << "5555999 " << library_str << frg::endlog;
 		object->dependencies.push(library);
-		mlibc::panicLogger() << "5555nnnn " << library_str << frg::endlog;
+		mlibc::infoLogger() << "5555nnnn " << library_str << frg::endlog;
 		if (library->wasVisited)
 			continue;
-		mlibc::panicLogger() << "55556666 " << library_str << frg::endlog;
+		mlibc::infoLogger() << "55556666 " << library_str << frg::endlog;
 		library->wasVisited = true;
 		dependencyQueue.push_back(library);
-		mlibc::panicLogger() << "5555555 " << library_str << frg::endlog;
+		mlibc::infoLogger() << "5555555 " << library_str << frg::endlog;
 	}
-	mlibc::panicLogger() << "5555111 " << frg::endlog;
+	mlibc::infoLogger() << "5555111 " << frg::endlog;
 }
 
 void ObjectRepository::_addLoadedObject(SharedObject *object) {
