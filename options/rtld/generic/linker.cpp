3e1296041d080bbe232cd4e8e1cc22673dbb2b54
cpplover0 <osdev555@yandex.com>
2026-08-06 21:38:18 +0300

    ggg

--- options/rtld/generic/linker.cpp
+++ options/rtld/generic/linker.cpp
@@ -1358,7 +1358,7 @@ void doInitialize(SharedObject *object) {
 		mlibc::infoLogger() << "rtld: Running DT_INIT_ARRAY functions" << frg::endlog;
 	__ensure((object->initArraySize % sizeof(InitFuncPtr)) == 0);
 	for(size_t i = 0; i < object->initArraySize / sizeof(InitFuncPtr); i++) {
-			mlibc::infoLogger() << "rtld: running func " << object->initArray[i] << frg::endlog;
+			mlibc::infoLogger() << "rtld: running func " << (void*)object->initArray[i] << frg::endlog;
 			object->initArray[i]();
 	}
 
