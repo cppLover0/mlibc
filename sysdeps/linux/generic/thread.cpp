27fdf015546669eae52f1c98f18144012291e1cb
cpplover0 <osdev555@yandex.com>
2026-08-07 23:42:13 +0300

    secure_getenv fix

--- sysdeps/linux/generic/thread.cpp
+++ sysdeps/linux/generic/thread.cpp
@@ -25,7 +25,7 @@ extern "C" void __mlibc_enter_thread(void *entry, void *user_arg) {
 
 namespace mlibc {
 
-static constexpr size_t default_stacksize = 0x200000;
+static constexpr size_t default_stacksize = 0x800000;
 
 int Sysdeps<PrepareStack>::operator()(void **stack, void *entry, void *user_arg, void *tcb, size_t *stack_size, size_t *guard_size, void **stack_base) {
 	(void)tcb;
