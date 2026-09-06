4769f8f3b00f07ab821d0bbb6da7f1e829fbd7c7
cpplover0 <osdev555@yandex.com>
2026-09-06 11:59:43 +0300

    nllang bug fixes

--- options/internal/generic/locale.cpp
+++ options/internal/generic/locale.cpp
@@ -831,7 +831,7 @@ char *nl_langinfo_l(nl_item item, localeinfo * loc) {
 			return const_cast<char *>(l->ctype.get(CODESET).asString().data());
 
 		case _NL_TIME_CODESET:
-		case ABDAY_1 ... ALT_DIGITS:
+		case ABDAY_1 ... _DATE_FMT:
 			return const_cast<char *>(l->time.get(item).asString().data());
 
 		case DECIMAL_POINT:
